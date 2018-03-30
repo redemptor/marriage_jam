f0e1618ca04230d967483495fc2e9c1cf0cf2ff1
Marcelo Belkiman <marcelobelkiman@gmail.com>
2018-03-30 17:45:19 -0300

    organize player code, now have states to player

---
+++ Solution/MarriageJam/Assets/Scripts/GlobalFields.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalFields : MonoBehaviour
+{
+    public enum MAP_TYPE { WALL, GROUND }
+}
--- Solution/MarriageJam/Assets/Scripts/Player.cs
+++ Solution/MarriageJam/Assets/Scripts/Player.cs
@@ -2,19 +2,21 @@
 
 public class Player : MonoBehaviour
 {
+    enum PLAYER_STATE { IDLE, WALK, JUMP, FALLING, DIE }
+    enum PLAYER_ANIM_STATE { IDLE, WALK, JUMP, PUNCH01 }
+    enum PLAYER_COLLIDERS { PlayerTop, PlayerBottom, PlayerLeft, PlayerRight }
+
     private bool _IsCollideTop = false;
+    private bool _IsCollideBottom = false;
     private bool _IsCollideRight = false;
-    private bool _IsJump = false;
-    private bool _IsFalling = false;
+    private bool _IsCollideLeft = false;
     private Animator _Animator;
+    private PLAYER_STATE _PlayerState = PLAYER_STATE.IDLE;
 
     public int MaxJumpPower = -2;
     public int MaxMoveVelocity;
     public int MoveVelocity;
     public int JumpVelocity;
-
-    //TODO : Remover os boleanos de estado e usar o enum
-    enum PLAYER_STATE { IDLE, MOVE, JUMP, FALLING, DIE }
 
     void Start()
     {
@@ -23,27 +25,52 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        _Animator.SetBool("jump", _IsJump);
+        _Animator.SetBool(PLAYER_ANIM_STATE.IDLE.ToString(), IsIddle());
+        _Animator.SetBool(PLAYER_ANIM_STATE.WALK.ToString(), IsWalk());
+        _Animator.SetBool(PLAYER_ANIM_STATE.JUMP.ToString(), IsFalling());
+        _Animator.SetBool(PLAYER_ANIM_STATE.JUMP.ToString(), IsJump());
     }
 
     void FixedUpdate()
     {
         Vector3 move = Vector3.zero;
         move.x = (Input.GetAxis("Horizontal") * MoveVelocity) * Time.deltaTime;
+        move.y = (Input.GetAxis("Vertical") * JumpVelocity) * Time.deltaTime;
 
-        if (!_IsJump)
+        switch (_PlayerState)
         {
-            move.y = (Input.GetAxis("Vertical") * JumpVelocity) * Time.deltaTime;
-        }
-        else
-        {
-            if (transform.position.z <= MaxJumpPower)
-            {
-                _IsFalling = true;
-            }
+            case PLAYER_STATE.IDLE:
+                if (move != Vector3.zero)
+                { _PlayerState = PLAYER_STATE.WALK; }
 
-            if (_IsFalling)
-            {
+                if (Input.GetButton("Jump"))
+                { _PlayerState = PLAYER_STATE.JUMP; }
+                break;
+            case PLAYER_STATE.WALK:
+                if (move == Vector3.zero)
+                { _PlayerState = PLAYER_STATE.IDLE; }
+
+                if (Input.GetButton("Jump"))
+                { _PlayerState = PLAYER_STATE.JUMP; return; }
+
+                if (move != Vector3.zero)
+                { _PlayerState = PLAYER_STATE.WALK; }
+
+                if (_IsCollideTop && move.y > 0) { move.y = 0; }
+                if (_IsCollideBottom && move.y < 0) { move.y = 0; }
+                if (_IsCollideRight && move.x > 0) { move.x = 0; }
+                if (_IsCollideLeft && move.x < 0) { move.x = 0; }
+                break;
+            case PLAYER_STATE.JUMP:
+                move.y = 0;
+
+                if (transform.position.z <= MaxJumpPower)
+                { _PlayerState = PLAYER_STATE.FALLING; return; }
+
+                move.y = (move.y + JumpVelocity) * Time.deltaTime;
+                move.z = (move.z - JumpVelocity) * Time.deltaTime;
+                break;
+            case PLAYER_STATE.FALLING:
                 if (transform.position.z < 0)
                 {
                     move.y = (move.y - JumpVelocity) * Time.deltaTime;
@@ -51,31 +78,10 @@ public class Player : MonoBehaviour
                 }
                 else
                 {
-                    _IsFalling = false;
-                    _IsJump = false;
+                    _PlayerState = PLAYER_STATE.IDLE;
                     move.z = 0;
                 }
-            }
-            else
-            {
-                move.y = (move.y + JumpVelocity) * Time.deltaTime;
-                move.z = (move.z - JumpVelocity) * Time.deltaTime;
-            }
-        }
-
-        if (Input.GetButton("Jump") && !_IsJump)
-        {
-            _IsJump = true;
-        }
-
-        if (!_IsJump)
-        {
-            if (_IsCollideTop && move.y > 0)
-                move.y = 0;
-
-
-            if (_IsCollideRight && move.x > 0)
-                move.x = 0;
+                break;
         }
 
         transform.Translate(move);
@@ -83,31 +89,49 @@ public class Player : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.name == "wall")
+        if (collision.gameObject.name.Equals(GlobalFields.MAP_TYPE.WALL.ToString()))
         {
-            if (collision.otherCollider.gameObject.name.Equals("PlayerTop"))
-            {
-                _IsCollideTop = true;
-            }
-            if (collision.otherCollider.gameObject.name.Equals("PlayerRight"))
-            {
-                _IsCollideRight = true;
-            }
+            if (collision.otherCollider.gameObject.name.Equals(PLAYER_COLLIDERS.PlayerTop.ToString()))
+            { _IsCollideTop = true; }
+            if (collision.otherCollider.gameObject.name.Equals(PLAYER_COLLIDERS.PlayerBottom.ToString()))
+            { _IsCollideBottom = true; }
+            if (collision.otherCollider.gameObject.name.Equals(PLAYER_COLLIDERS.PlayerRight.ToString()))
+            { _IsCollideRight = true; }
+            if (collision.otherCollider.gameObject.name.Equals(PLAYER_COLLIDERS.PlayerLeft.ToString()))
+            { _IsCollideLeft = true; }
         }
     }
 
     void OnCollisionExit2D(Collision2D collision)
     {
-        if (collision.gameObject.name == "wall")
+        if (collision.gameObject.name.Equals(GlobalFields.MAP_TYPE.WALL.ToString()))
         {
-            if (collision.otherCollider.gameObject.name.Equals("PlayerTop"))
-            {
-                _IsCollideTop = false;
-            }
-            if (collision.otherCollider.gameObject.name.Equals("PlayerRight"))
-            {
-                _IsCollideRight = false;
-            }
+            if (collision.otherCollider.gameObject.name.Equals(PLAYER_COLLIDERS.PlayerTop.ToString()))
+            { _IsCollideTop = false; }
+            if (collision.otherCollider.gameObject.name.Equals(PLAYER_COLLIDERS.PlayerBottom.ToString()))
+            { _IsCollideBottom = false; }
+            if (collision.otherCollider.gameObject.name.Equals(PLAYER_COLLIDERS.PlayerRight.ToString()))
+            { _IsCollideRight = false; }
+            if (collision.otherCollider.gameObject.name.Equals(PLAYER_COLLIDERS.PlayerLeft.ToString()))
+            { _IsCollideLeft = false; }
         }
     }
+
+    public bool IsJump()
+    {
+        return _PlayerState == PLAYER_STATE.JUMP;
+    }
+    public bool IsWalk()
+    {
+        return _PlayerState == PLAYER_STATE.WALK;
+    }
+    public bool IsIddle()
+    {
+        return _PlayerState == PLAYER_STATE.IDLE;
+    }
+    public bool IsFalling()
+    {
+        return _PlayerState == PLAYER_STATE.FALLING;
+    }
+
 }
