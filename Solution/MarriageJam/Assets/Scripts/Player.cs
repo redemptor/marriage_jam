a5f815aceec0aa2d5c760b8481ad6c5c4aae8c9a
redemptor <redemptor@live.com>
2018-03-31 15:21:36 -0300

    ajuste animator player

--- Solution/MarriageJam/Assets/Scripts/Player.cs
+++ Solution/MarriageJam/Assets/Scripts/Player.cs
@@ -43,94 +43,17 @@ public class Player : Actor
             Flip();
         }
 
-
-        //switch (_PlayerState)
-        //{
-        //    case PLAYER_STATE.IDLE:
-        //        if (move != Vector3.zero)
-        //        {
-        //            _PlayerState = PLAYER_STATE.WALK;
-        //        }
-
-        //        break;
-
-        //    case PLAYER_STATE.WALK:
-        //        if (move == Vector3.zero)
-        //        {
-        //            _PlayerState = PLAYER_STATE.IDLE;
-        //        }
-
-        //        if (Input.GetButtonDown(GlobalFields.BUTTONS.ACTION.ToString()))
-        //        {
-        //            _PlayerState = PLAYER_STATE.ATTACK;
-        //            return;
-        //        }
-
-
-        //        if (move != Vector3.zero)
-        //        {
-        //            _PlayerState = PLAYER_STATE.WALK;
-        //        }
-
-        //        break;
-
-        //    case PLAYER_STATE.ATTACK:
-
-
-
-        //        //if (_NeedUpdateAttackAnimation)
-        //        //{
-        //        //    switch (_PlayerAttackState)
-        //        //    {
-        //        //        case PLAYER_ANIM_STATE.IDLE:
-        //        //            _PlayerAttackState = PLAYER_ANIM_STATE.PUNCH01;
-        //        //            _TimeReleaseCombo = 0;
-        //        //            break;
-        //        //        case PLAYER_ANIM_STATE.PUNCH01:
-        //        //            _PlayerAttackState = PLAYER_ANIM_STATE.PUNCH02;
-        //        //            _TimeReleaseCombo = 0;
-        //        //            break;
-        //        //        case PLAYER_ANIM_STATE.PUNCH02:
-        //        //            _PlayerAttackState = PLAYER_ANIM_STATE.PUNCH03;
-        //        //            _TimeReleaseCombo = 20;
-        //        //            break;
-        //        //    }
-        //        //    _NeedUpdateAttackAnimation = false;
-        //        //}
-        //        //else
-        //        //{
-        //        //    if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1
-        //        //        && !animator.IsInTransition(0))
-        //        //    {
-        //        //        _NeedUpdateAttackAnimation = true;
-        //        //        _PlayerState = PLAYER_STATE.IDLE;
-        //        //    }
-        //        //}
-
-        //        break;
-        //}
-
-
-
         rigidbody2D.velocity = move;
-
-        //ControlAttackCombo();
     }
 
     void SetAnimation()
     {
-        //animator.SetBool(PLAYER_ANIM_STATE.IDLE.ToString(), IsIddle());
         animator.SetBool(PLAYER_ANIM_STATE.WALK.ToString(), IsWalk());
         animator.SetInteger("ATTACK", comboHit);
-
-        //animator.SetBool(PLAYER_ANIM_STATE.PUNCH01.ToString(), IsPunch01());
-        //animator.SetBool(PLAYER_ANIM_STATE.PUNCH02.ToString(), IsPunch02());
-        //animator.SetBool(PLAYER_ANIM_STATE.PUNCH03.ToString(), IsPunch03());
     }
 
     private void Attack()
     {
-        //_PlayerState = PLAYER_STATE.ATTACK;
         comboHit++;
 
         if (comboHit > maxHitCombo)
@@ -142,26 +65,11 @@ public class Player : Actor
         timeNextHit = timeNextAttack;
     }
 
-    private void ControlAttackCombo()
-    {
-        //if (_PlayerState == PLAYER_STATE.IDLE
-        //    && _PlayerAttackState != PLAYER_ANIM_STATE.IDLE)
-        //{
-        //    _TimeReleaseCombo++;
-        //    if (_TimeReleaseCombo >= 20)
-        //    {
-        //        _NeedUpdateAttackAnimation = true;
-        //        _PlayerState = PLAYER_STATE.IDLE;
-        //        _PlayerAttackState = PLAYER_ANIM_STATE.IDLE;
-        //    }
-        //}
-    }
 
 
     public bool IsWalk()
     {
         return rigidbody2D.velocity != Vector2.zero;
-        //return _PlayerState == PLAYER_STATE.WALK;
     }
 
     //public bool IsIddle()
