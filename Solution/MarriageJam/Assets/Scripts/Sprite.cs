bd4931e06211fd2d5c1330a1d0c41542c183f1a7
redemptor <redemptor@live.com>
2018-03-31 00:55:25 -0300

    Script para definir profundidade do sprite na cena

---
+++ Solution/MarriageJam/Assets/Scripts/Sprite.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SpriteRenderer))]
+[ExecuteInEditMode]
+public class Sprite : MonoBehaviour
+{
+    public bool isStatic;
+
+    private SpriteRenderer spriteRenderer;
+    private Collider2D collider;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        collider = GetComponent<Collider2D>();
+
+        SortingOrder();
+    }
+
+    void LateUpdate()
+    {
+        if (!isStatic)
+        {
+            SortingOrder();
+        }
+    }
+
+    void SortingOrder()
+    {
+        var reference = (isStatic && collider != null)
+            ? collider.bounds.max
+            : (collider != null
+                ? collider.bounds.min
+                : spriteRenderer.bounds.min);
+
+        //spriteRenderer.sortingOrder = (int)(Camera.main.ViewportToWorldPoint(reference).y * -10);
+        spriteRenderer.sortingOrder = (int)(reference.y * -100);
+    }
+}
