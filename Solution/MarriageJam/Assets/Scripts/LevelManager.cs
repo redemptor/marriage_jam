c7eb8474f2af9ea1ff78577f06c3cef41aa320f0
Marcelo Belkiman <marcelo.mesquita@verisoft.com.br>
2018-04-04 10:12:53 -0300

    Merge branch 'master' of https://github.com/redemptor/marriage_jam

---
+++ Solution/MarriageJam/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name){
+		Debug.Log ("Scene carregada: " + name);
+		SceneManager.LoadScene(name);
+	}
+
+	public void QuitRequest(){
+		Debug.Log ("Sair");
+		Application.Quit ();
+	}
+
+}
---
+++ Solution/MarriageJam/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+	static MusicPlayer instance = null;
+	
+	void Start () {
+		if (instance != null && instance != this) {
+			Destroy (gameObject);
+			print ("Musica duplicada é deletada!");
+		} else {
+			print ("Se é a primeira música a torna persistente!");
+			instance = this;
+			GameObject.DontDestroyOnLoad(gameObject);
+		}
+		
+	}
+}
