44c77483635537e9ef67ed9386b2227f003609fd
raphael <raphael.silva@live.com>
2018-04-08 00:11:07 -0300

    Adjustment Collectable Items and add shadow

--- Solution/MarriageJam/Assets/Scripts/Collectable.cs
+++ Solution/MarriageJam/Assets/Scripts/Collectable.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using UnityEngine;
 
 public class Collectable : MonoBehaviour
 {
@@ -6,10 +7,21 @@ public class Collectable : MonoBehaviour
 
     private Vector3 currentPosition;
     private new Collider2D collider;
+    private SpriteRenderer item;
+    private SpriteRenderer shadow;
 
     private void Start()
     {
         collider = GetComponent<Collider2D>();
+
+        var sprites = GetComponentsInChildren<SpriteRenderer>();
+        item = sprites.FirstOrDefault(x => x.name.Equals("Item"));
+        shadow = sprites.FirstOrDefault(x => x.name.Equals("Shadow"));
+
+        if (item == null)
+        {
+            item = sprites[0];
+        }
 
         curve = new AnimationCurve(new Keyframe(0, 0), new Keyframe(0.8f, 0.05f))
         {
@@ -17,16 +29,19 @@ public class Collectable : MonoBehaviour
             postWrapMode = WrapMode.PingPong
         };
 
-        currentPosition = transform.position;
+        currentPosition = item.transform.localPosition;
     }
 
     private void Update()
     {
-        transform.position = new Vector3(
+        item.transform.localPosition = new Vector3(
             currentPosition.x,
             currentPosition.y + curve.Evaluate(Time.time),
             currentPosition.z);
 
-        collider.offset = new Vector2(0f, -curve.Evaluate(Time.time));
+        var shadowScale = 1 - curve.Evaluate(Time.time) * 4f;
+        shadow.transform.localScale = new Vector3(shadowScale, shadowScale, shadow.transform.localScale.z);
+
+        //collider.offset = new Vector2(0f, -curve.Evaluate(Time.time));
     }
 }
