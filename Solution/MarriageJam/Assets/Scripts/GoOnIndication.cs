3fb9a6eba4c593f53779f43d81afd79f3beb7a2f
Trackless29 <adrianomoreira29@gmail.com>
2018-04-10 01:56:58 -0300

    UI Go system

---
+++ Solution/MarriageJam/Assets/Scripts/GoOnIndication.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoOnIndication : MonoBehaviour
+{
+	public GameObject goImage;
+	[Range(2,10)]
+	public int blinkCycle = 4;
+	[Range(0.1f,0.5f)]
+	public float blinkDelay = 0.3f;
+	private bool activeSignaling = true;
+
+	private void Start()
+	{
+		goImage.SetActive(false);	
+	}
+	
+	public void TurnOnSignaling()
+	{
+		if(goImage != null && activeSignaling)
+		{
+			StartCoroutine(BlinkImage(blinkDelay));
+			activeSignaling = false;
+		}
+	}
+
+	IEnumerator BlinkImage(float delay)
+	{
+		for (int i = 0; i < blinkCycle; i++)
+		{
+            yield return new WaitForSeconds(delay);
+			goImage.SetActive(true);
+			yield return new WaitForSeconds(delay);
+			goImage.SetActive(false);
+		}
+		goImage.SetActive(false);
+		activeSignaling = true;
+	}
+}
