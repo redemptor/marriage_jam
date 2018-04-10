5a28c44910e4a882cba8c5d73de4ab9d3fa2b7f6
luiz-dev <gustavo.basic@gmail.com>
2018-04-10 06:40:55 -0300

    changes to the start screen and fixed resolution

---
+++ Solution/MarriageJam/Assets/Scripts/StartMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour {
+
+	Renderer r;
+	// Use this for initialization
+	void Start () {
+		r = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.anyKey){
+			StartCoroutine("fadeOut");
+		}
+	}
+
+	IEnumerator fadeOut(){
+		for(float f = 1f; f >= 0f; f -= 0.01f){
+			Color c = r.material.color;
+			c.a = f;
+			r.material.color = c;
+
+			yield return null;
+		}
+		sceneChange();
+	}
+
+	void sceneChange(){
+		SceneManager.LoadScene("Level 1");
+	}
+}
