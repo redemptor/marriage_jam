c63db089dd47811a06e8a40c2a47a02dcab34946
redemptor <redemptor@live.com>
2018-04-10 16:20:57 -0300

    press start button animation

--- Solution/MarriageJam/Assets/Scripts/StartMenu.cs
+++ Solution/MarriageJam/Assets/Scripts/StartMenu.cs
@@ -3,34 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class StartMenu : MonoBehaviour {
+public class StartMenu : MonoBehaviour
+{
+    public SpriteRenderer pressStartButton;
 
-	Renderer r;
-	// Use this for initialization
-	void Start () {
-		r = GetComponent<Renderer>();
+    Renderer r;
+    // Use this for initialization
+    void Start()
+    {
+        r = GetComponent<Renderer>();
         SoundManager.instance.PlayMusicMenu();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if(Input.anyKey){
-			StartCoroutine("fadeOut");
-		}
-	}
+    }
 
-	IEnumerator fadeOut(){
-		for(float f = 1f; f >= 0f; f -= 0.01f){
-			Color c = r.material.color;
-			c.a = f;
-			r.material.color = c;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            if(pressStartButton != null)
+            {
+                pressStartButton.gameObject.SetActive(false);
+            }
 
-			yield return null;
-		}
-		sceneChange();
-	}
+            StartCoroutine("fadeOut");
+        }
+    }
 
-	void sceneChange(){
-		SceneManager.LoadScene("Level 1");
-	}
+    IEnumerator fadeOut()
+    {
+        for (float f = 1f; f >= 0f; f -= 0.01f)
+        {
+            Color c = r.material.color;
+            c.a = f;
+            r.material.color = c;
+
+            yield return null;
+        }
+        sceneChange();
+    }
+
+    void sceneChange()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
 }
